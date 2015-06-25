1cef8e1714b4fbadb214403cf4e1acd1545518fb
Grace Hatamyar <hatamyarg@gmail.com>
2015-06-24 20:14:01 -0700

    add all function to entry and related test

--- journal.py
+++ journal.py
@@ -40,6 +40,12 @@ class Entry(Base):
         session.add(instance)
         return instance
 
+    @classmethod
+    def all(cls, session=None):
+        if session is None:
+            session = DBSession
+        return session.query(cls).order_by(cls.created.desc()).all()
+
 
 def init_db():
     engine = sa.create_engine(DATABASE_URL, echo=True)
--- test_journal.py
+++ test_journal.py
@@ -84,4 +84,25 @@ def test_entry_no_text_fails(db_session):
         db_session.flush()
 
 
-  
+def test_read_entries_empty(db_session):
+    entries = journal.Entry.all()
+    assert len(entries) == 0
+
+
+def test_read_entries_one(db_session):
+    title_template = "Title {}"
+    text_template = "Entry Text {}"
+
+    # write three entries, with order clear in the title and text
+    for x in range(3):
+        journal.Entry.write(
+            title=title_template.format(x),
+            text=text_template.format(x),
+            session=db_session)
+        db_session.flush()
+    entries = journal.Entry.all()
+    assert len(entries) == 3
+    assert entries[0].title > entries[1].title > entries[2].title
+    for entry in entries:
+        assert isinstance(entry, journal.Entry)
+        
