1fb23a0ca04aa5a19949c1c80eb9264b5cb4ecba
Grace Hatamyar <hatamyarg@gmail.com>
2015-07-04 11:58:34 -0700

    move test fixtures to conftest

---
+++ tests/conftest.py
@@ -0,0 +1,49 @@
+import os
+import pytest
+from pytest_bdd import given
+import journal
+from sqlalchemy import create_engine
+
+
+TEST_DATABASE_URL = os.environ.get(
+    'DATABASE_URL',
+    'postgresql://gracehatamyar@localhost:5432/test-learning-journal'
+    )
+
+
+@pytest.fixture(scope='session')
+def connection(request):
+    engine = create_engine(TEST_DATABASE_URL)
+    journal.Base.metadata.create_all(engine)
+    # create connection to our database
+    # this opens a transaction that last for the scope
+    # of the entire session
+    connection = engine.connect()
+    journal.DBSession.registry.clear()
+    # bind this in the name space  of journal
+    journal.DBSession.configure(bind=connection)
+    journal.Base.metadata.bind = engine
+    request.addfinalizer(journal.Base.metadata.drop_all)
+    return connection
+
+
+@pytest.fixture()
+def db_session(request, connection):
+    # starts a new transaction inside the already open transaction
+    from transaction import abort
+    trans = connection.begin()
+    # every test has a transaction that's open for the duration
+    # of the test, and rollsback when the test is completed
+    request.addfinalizer(trans.rollback)
+    request.addfinalizer(abort)
+
+    from journal import DBSession
+    return DBSession
+
+
+@pytest.fixture()
+def app(db_session):
+    from journal import main
+    from webtest import TestApp
+    app = main()
+    return TestApp(app)
