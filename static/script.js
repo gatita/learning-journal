d0abd9b10afb5fa75cf5b3800cc761a2ee836c90
Grace Hatamyar <hatamyarg@gmail.com>
2015-08-18 21:49:18 -0700

    add twitter button and include widgets.js; refs #14

--- static/script.js
+++ static/script.js
@@ -1,6 +1,23 @@
 
 $(function() {
 
+    window.twttr = (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0],
+        t = window.twttr || {};
+        if (d.getElementById(id)) return t;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://platform.twitter.com/widgets.js";
+        fjs.parentNode.insertBefore(js, fjs);
+ 
+        t._e = [];
+        t.Ready = function(f) {
+            t._e.push(f);
+        };
+ 
+        return t;
+    }(document, "script", "twitter-wjs"));
+
     $('#add-new').click(function(e){
         e.preventDefault();
         $('header').css('width', '35%');
